136fddfca97bdfabec3812a2f118f4752a026358
David Wallace Croft <david@CroftSoft.com>
2023-03-05 19:08:11 -0600

    updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,61 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
-const dist = path.resolve(__dirname, "dist");
-
 module.exports = {
-  mode: "production",
-  entry: {
-    index: "./js/index.js"
-  },
-  output: {
-    path: dist,
-    filename: "[name].js"
-  },
   devServer: {
-    contentBase: dist,
+    allowedHosts: ['localhost'],
+    client: {
+      logging: 'verbose',
+      overlay: true,
+      progress: true,
+    },
+    open: true,
+    static: false,
+  },
+  entry: {
+    index: './js/entry-index.js'
+  },
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  mode: 'production',
+  output: {
+    filename: 'croftsoft-walk-the-dog.js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new CopyPlugin([
-      path.resolve(__dirname, "static")
+      path.resolve(__dirname, 'static')
     ]),
-
     new WasmPackPlugin({
       crateDirectory: __dirname,
     }),
   ]
 };
+
+
+
+// const dist = path.resolve(__dirname, "dist");
+
+// module.exports = {
+//   mode: "production",
+//   entry: {
+//     index: "./js/index.js"
+//   },
+//   output: {
+//     path: dist,
+//     filename: "[name].js"
+//   },
+//   devServer: {
+//     contentBase: dist,
+//   },
+//   plugins: [
+//     new CopyPlugin([
+//       path.resolve(__dirname, "static")
+//     ]),
+
+//     new WasmPackPlugin({
+//       crateDirectory: __dirname,
+//     }),
+//   ]
+// };
